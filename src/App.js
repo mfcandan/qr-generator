ab479f1b309662420de9ff3735943168ddd605b1
Fatih Candan <mfatihcandan@gmail.com>
2023-09-01 09:55:36 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import QrCode from "./QrCode";
 function App() {
   return (
     <div className="App">
-      <h2 style={{ marginBottom: "0.5em" }}>Denizin QrCode JENERATÖRÜ</h2>
+      <h2 style={{ marginBottom: "0.5em" }}>QrCode Generator</h2>
       <QrCode />
     </div>
   );
